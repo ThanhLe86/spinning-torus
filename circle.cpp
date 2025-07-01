72f9df06c851fa2ae925ce622a8ab131cd383066
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-07-01 19:38:50 +0700

    fix: code documentation

--- circle.cpp
+++ circle.cpp
@@ -12,21 +12,27 @@ int main(int argc, char const *argv[])
     
     cout << "Coords: ";
 
-    //basically, we have the OG coords as:
-    // x = R*cos(phi)
-    // y = R*sin(phi)
-    // z = 0
-
     for(double rotation_angle = 0; rotation_angle < pi; rotation_angle += pi/180){ //animating rotate for 180 degrees around x axis
         for(double phi = 0; phi <= 2*pi; phi += pi/180){ 
-            double x = R*cos(phi), y = R*sin(phi)*cos(rotation_angle) - 0*sin(rotation_angle), z = R*sin(phi)*sin(rotation_angle) + 0*cos(rotation_angle);
+            vector circlePoints(R*cos(phi), R*sin(phi)*cos(rotation_angle) - 0*sin(rotation_angle), R*sin(phi)*sin(rotation_angle) + 0*cos(rotation_angle));
             for(double theta = 0; theta <= 2*pi; theta += pi/100){
-                double u = r*cos(theta) + x, v = r*sin(theta) + y, w = 0; //TODO: change this, to compute the coordinates of a point in  3d space
+                vector torusPoints(circlePoints.x*cos(theta) + circlePoints.x, r*sin(theta) + circlePoints.y, 0); //TODO: change this, to compute the coordinates of a point in  3d space
 
-                cout << "( " << u*cos(pi/2) << ", " << v << ", " << u * sin(pi/2) << " )";
+                cout << "( " << torusPoints.x*cos(pi/2) << ", " << torusPoints.y << ", " << torusPoints.z * sin(pi/2) << " )";
             }
         }
     }
     
     return 0;
 }
+
+
+//TODO: properly document code in a markdown
+//basically, we have the OG coords of the torus circle:
+// x = R*cos(phi)
+// y = R*sin(phi)
+// z = 0
+
+//phi: angle for rotating around the center 
+//theta: angle for rotating around each point on the main torus circle
+//rotation_angle: main rotation angle for rotating the entire torus
